52418df13c2fbdd937bb2b886c0abd629bbb11f8
Shikhar bajpai <142872564+shikhar-blaziee@users.noreply.github.com>
2026-03-29 11:21:53 +0530

    Python Pratice

---
+++ Python/typeConversion.py
@@ -0,0 +1,16 @@
+# comments
+# this will print hello world
+print("hello world")
+''' this is a multi line comment'''
+
+
+''' ***TYPECONVERSION*** '''
+AGE="45";
+print(type(AGE))
+print(int(AGE)+5)
+
+price= "199.5"
+print(float(price)+5)
+
+agee=56;
+print(str(agee))
