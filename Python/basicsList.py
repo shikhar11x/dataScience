d1134ac7a6867a11b64ea78b4d8e4e030715e8f7
Shikhar bajpai <142872564+shikhar-blaziee@users.noreply.github.com>
2026-03-29 22:12:26 +0530

    today python practice

---
+++ Python/basicsList.py
@@ -0,0 +1,24 @@
+# name=["hello","hey","hii","halo","hola"]
+# print(name)
+# print(type(name))
+# print(name[1])
+# print(name[0:2])
+# print(len(name))
+# name[4]="hii"
+# print(name)
+
+
+items=["apple", "banana","orange"]
+print(items)
+items[0]="grapes"
+print(items)
+items.append('strawbeeries')
+print(items)
+items.extend(["banana","pineapples"])
+print(items)
+items.remove("banana")
+print(items)
+items.pop(0)
+print(items)
+print(items.index("banana"))
+print(items.count("banana"))
