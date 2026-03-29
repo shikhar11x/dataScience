d52d8ca4193a4ee4e3ba1456697eb4e3c956324e
Shikhar bajpai <142872564+shikhar-blaziee@users.noreply.github.com>
2026-03-29 21:29:10 +0530

    today python practice

--- Python/basics1.py
+++ Python/basics1.py
@@ -4,25 +4,25 @@ print("*** hello "+ name +" ***")
 # Always return a string 
 # must convert to int or float
 
-'''****Operatos**** 
-types
-1. Arithmetic Operators {+ / * - // (floor division) % (modulus) **(Exponentiation)}
-2. comparison { == equal != not equals > greater than < less than >= greater/equals <= less/equal}
-3. assignment { = += (x=x+5 same as x+=5) -= *= }
-4. logical {and(t and t = t) or (t or f = t) not }
-5. membership {in  not in}
-5. identity {== (values are same) is(they are the same object in memory)}
-6. bitwise 
+# '''****Operatos**** 
+# types
+# 1. Arithmetic Operators {+ / * - // (floor division) % (modulus) **(Exponentiation)}
+# 2. comparison { == equal != not equals > greater than < less than >= greater/equals <= less/equal}
+# 3. assignment { = += (x=x+5 same as x+=5) -= *= }
+# 4. logical {and(t and t = t) or (t or f = t) not }
+# 5. membership {in  not in}
+# 5. identity {== (values are same) is(they are the same object in memory)}
+# 6. bitwise 
 
 
-operator precedence 
-()
-**
-* / // %
-+ -
-== != > <
-not and or 
-'''
+# operator precedence 
+# ()
+# **
+# * / // %
+# + -
+# == != > <
+# not and or 
+# '''
 
 a= 10
 b=5
---
+++ Python/basicsString.py
@@ -0,0 +1,23 @@
+name="blaze"
+#-5 -4 -3 -2 -1 reverse slicing
+# 0  1  2  3  4 slicing 
+print(name[0])
+# name[0]=h;#not allowed
+print(name.upper())
+print(name.strip()) #removes extra spaces
+print(name.replace("ze","zi"))
+print(name.isalpha())
+print(name.isnumeric())
+
+multiiiLine='''helooo
+hello 
+hello
+helo'''
+
+print(len(multiiiLine))
+
+
+print(name[0:3])
+print(name[0:-1])
+
+print(f"5+4 = {5+4} * 9 = {9*9}")
