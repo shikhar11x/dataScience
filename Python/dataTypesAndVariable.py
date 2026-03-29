8604d9f4696c15436aa8cdb59403de6b1301877c
Shikhar bajpai <142872564+shikhar-blaziee@users.noreply.github.com>
2026-03-29 11:07:26 +0530

    Python Pratice

---
+++ Python/dataTypesAndVariable.py
@@ -0,0 +1,10 @@
+a = 23; #integer int
+price= 5.55 # float 
+name = "blaze" #String
+is_admin= True; #boolean
+is_blocked=False; #boolean
+
+print(type(a))
+print(type(price))
+print(type(name))
+print(type(is_admin))
