b23544d3710b6b2e832328b21887d1db2b00441c
Shikhar bajpai <142872564+shikhar-blaziee@users.noreply.github.com>
2026-03-29 12:12:51 +0530

    Python Pratice

---
+++ Python/basics1.py
@@ -0,0 +1,50 @@
+print("Hey this is a program")
+name=input("enter your name - ")
+print("*** hello "+ name +" ***")
+# Always return a string 
+# must convert to int or float
+
+'''****Operatos**** 
+types
+1. Arithmetic Operators {+ / * - // (floor division) % (modulus) **(Exponentiation)}
+2. comparison { == equal != not equals > greater than < less than >= greater/equals <= less/equal}
+3. assignment { = += (x=x+5 same as x+=5) -= *= }
+4. logical {and(t and t = t) or (t or f = t) not }
+5. membership {in  not in}
+5. identity {== (values are same) is(they are the same object in memory)}
+6. bitwise 
+
+
+operator precedence 
+()
+**
+* / // %
++ -
+== != > <
+not and or 
+'''
+
+a= 10
+b=5
+
+print(a+b)
+print(a*b)
+print(a-b)
+print(a/b)
+print(a//b)
+print(a%b)
+print(a**b)
+
+
+print(a==b)
+print(a!=b)
+print(a>b)
+print(a<b)
+print(a>=b)
+print(a<=b)
+
+
+print(True and True)
+print(True and False)
+print(True or False)
+print(True or True)
